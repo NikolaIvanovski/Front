d54de9057f15c94ee00d9659b73d3a54d7d794bf
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2021-01-21 14:40:49 +0200

    Update schema.sh

--- schema.sh
+++ schema.sh
@@ -6,7 +6,7 @@ body='{
 
 curl -s -X POST \
    -H "Accept: application/vnd.github.everest-preview+json" \
-   -H "Authorization: token 8a9b6f07689944957148cc4f96d0f94062b14685" \
+   -H "Authorization: token ${{ secrets.GITHUBTOKEN }}" \
    -d "$body" \
    https://api.github.com/repos/NikolaIvanovski/Base/dispatches
 
